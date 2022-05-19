40ad35d89b2fef089d5ac115075a9a9f82682a1d
jessicarydberg <jessica.rydberg@hotmail.se>
2022-05-19 14:41:07 +0000

    Add more tests

--- student.py
+++ student.py
@@ -1,16 +1,39 @@
 from datetime import date, timedelta
+import requests
 
 
 class Student:
     """A Student class as base for method testing"""
-    
+
     def __init__(self, first_name, last_name):
         self._first_name = first_name
         self._last_name = last_name
-        self._start_date = date_today()
-        self.end_date = date_today() + timedelta(days=365)
+        self._start_date = date.today()
+        self.end_date = date.today() + timedelta(days=365)
         self.naughty_list = False
 
     @property
     def full_name(self):
         return f"{self._first_name} {self._last_name}"
+
+    @property
+    def email(self):
+        lower_first = self._first_name.lower()
+        lower_last = self._last_name.lower()
+        return f"{lower_first}.{lower_last}@email.com"
+
+    def alert_santa(self):
+        self.naughty_list = True
+
+    def apply_extension(self, days):
+        self.end_date += timedelta(days=days)
+
+    def course_schedule(self):
+        response = requests.get(
+            f"http://company.com/course-schedule/{self._last_name}/{self._first_name}"
+        )
+
+        if response.ok:
+            return response.text
+        else:
+            return "Something went wrong with the request"
